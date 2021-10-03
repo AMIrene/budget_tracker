8ebd5bed3c0df37642cf450939fe1981b54f4c21
AMIrene <f.adammanik@gmail.com>
2021-10-03 15:54:56 +1100

    added fetch event

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,19 @@
+// const FILES_TO_CACHE = [
+//     "/",
+//     "/index.html",
+//     "/db.js",
+//     "/index.js",
+//     "/manifest.webmanifest",
+//     "/icons/icon-192x192.png",
+//     "/icons/icon-512x512.png",
+//     "/styles.css",
+
+// ];
+
+
+// const STATIC_CACHE = "static-cache-v1";
+// const RUNTIME_CACHE = "runtime-cache";
+
 //install service worker
 
 self.addEventListener('install', evt => {
@@ -9,5 +25,9 @@ self.addEventListener('install', evt => {
 
 self.addEventListener('activate', evt => {
     console.log('service worker activated');
-})
+});
 
+// fetch event
+self.addEventListener('fetch', evt => {
+    console.log('fetch event', evt);
+});
