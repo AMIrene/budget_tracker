86cf801ec5313366ac4cbde375c870d2db772443
AMIrene <f.adammanik@gmail.com>
2021-10-03 14:43:14 +1100

    service worker activated

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,3 +4,10 @@ self.addEventListener('install', evt => {
     console.log('service worker installed');
 
 });
+
+//service worker activated
+
+self.addEventListener('activate', evt => {
+    console.log('service worker activated');
+})
+
